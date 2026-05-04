60157be09dffb075fa5aa3fe64af4295eea392b6
kaue-hu <benzmerceds6365@gmail.com>
2026-05-04 14:45:06 -0300

    Create Programa.cs

---
+++ Projeto Produto/Programa.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+
+namespace _67_saborrrr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome:");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.Write("Digite o numero do produto a ser adicionado no estoqe");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados Atualizados: " + p);
+        }
+    }
+}
