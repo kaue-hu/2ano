ab5a0f9d8c999fbb1015dfdeec3c1c66039c4a3b
kaue-hu <benzmerceds6365@gmail.com>
2026-05-07 10:46:48 -0300

    Create Funcionario.cs

---
+++ Projeto Funcionario/Funcionario.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace exercicio_4
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido ()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentoSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);
+        }
+        public override string ToString()
+        {
+            return Nome
+            +", R$ "
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
